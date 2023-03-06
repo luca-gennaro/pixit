d6d5f20cd86da9527438c622d6de41bae51a6cb0
Luca Gennaro <lucagennaro91@gmail.com>
2023-03-06 21:35:24 +0100

    fix

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext} from 'react'
 import { InputContext } from '../Context/Context'
 
 const ColorPalette = ({colors}) => {
